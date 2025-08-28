e8975411700daef8d43b4381ca25a77e78952ee4
Mohammed-Sony <sonymohammad84@gmail.com>
2025-08-28 09:48:42 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,8 @@
+# Simple Python Program
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+year_when_100 = 2025 - age + 100
+
+print(f"Hello {name}, you will turn 100 years old in the year {year_when_100}.")
